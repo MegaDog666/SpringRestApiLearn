ae7f8f9f44b280f026766d10c2fe308a636db698
MegaDog666 <Moonandre9999@mail.ru>
2025-01-15 13:44:51 +0300

    Les 94 (REST API)
    Разобрали что такое DTO, с чем его едят и зачем он нужен)
    
    В общем и целом я понял, быть может сам не напишу такое, но думаю как раз вот буду писать потому что след урок 95 это ТЗ...
    Последнее ТЗ...
    Страшно очень, еще сегодня созвон по поводу компании (Надеюсь все норм будет)
    В общем и целом буду надеяться на лучшее)
    А так остался 1 урок (Вообще их больше но тут почему-то их столько-то)

--- src/main/java/org/example/firstRESTApp/controllers/PeopleController.java
+++ src/main/java/org/example/firstRESTApp/controllers/PeopleController.java
@@ -1,11 +1,13 @@
 package org.example.firstRESTApp.controllers;
 
 import jakarta.validation.Valid;
+import org.example.firstRESTApp.dto.PersonDTO;
 import org.example.firstRESTApp.models.Person;
 import org.example.firstRESTApp.service.PeopleService;
 import org.example.firstRESTApp.util.PersonErrorResponse;
 import org.example.firstRESTApp.util.PersonNotCreatedException;
 import org.example.firstRESTApp.util.PersonNotFountException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,31 +15,37 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/people")
 public class PeopleController {
 
     private final PeopleService peopleService;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public PeopleController(PeopleService peopleService) {
+    public PeopleController(PeopleService peopleService,
+                            ModelMapper modelMapper) {
         this.peopleService = peopleService;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping
-    public List<Person> getPeople() {
-        return peopleService.findAll(); // Jackson конвертирует эти объекты в JSON
+    public List<PersonDTO> getPeople() {
+        return peopleService.findAll().stream().map(this::converToPersonDTO)
+                .collect(Collectors.toList()); // Jackson конвертирует эти объекты в JSON
     }
 
     @GetMapping("/{id}")
-    public Person getPerson(@PathVariable("id") int id) {
-        return peopleService.findOne(id); // Jackson конвертирует этот объект в JSON
+    public PersonDTO getPerson(@PathVariable("id") int id) {
+        return converToPersonDTO(peopleService.findOne(id)); // Jackson конвертирует этот объект в JSON
     }
 
     @PostMapping
-    public ResponseEntity<HttpStatus> create(@RequestBody @Valid Person person,
+    public ResponseEntity<HttpStatus> create(@RequestBody @Valid PersonDTO personDTO,
                                              BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             StringBuilder errorMsg = new StringBuilder();
@@ -52,11 +60,13 @@ public class PeopleController {
             throw new PersonNotCreatedException(errorMsg.toString());
         }
 
-        peopleService.save(person);
+        peopleService.save(convertToPerson(personDTO));
 
         // Отправляем HTTP ответ с пустым телом и со статусом 200
         return ResponseEntity.ok(HttpStatus.OK);
     }
+
+
 
     @ExceptionHandler
     private ResponseEntity<PersonErrorResponse> handleException(PersonNotFountException e) {
@@ -79,4 +89,12 @@ public class PeopleController {
         // Будет в HTTP ответе тело ответа (response) и статус в заголовке
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST); // BAD_REQUEST - 400 статус
     }
+    private Person convertToPerson(PersonDTO personDTO) {
+        return modelMapper.map(personDTO, Person.class);
+    }
+
+    private PersonDTO converToPersonDTO(Person person) {
+        return modelMapper.map(person, PersonDTO.class);
+    }
+
 }
